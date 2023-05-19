1a7f55e15908cb9261d68037c6ec6836527a9f5d
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-19 14:42:51 +0530

    final

--- src/components/Ui.jsx
+++ src/components/Ui.jsx
@@ -6,25 +6,29 @@ import Web3 from 'web3';
 const Ui = () => {
 
     const [account, setAccount] = useState(null)
-    const [address, setAddress] = useState(null)
+    const[address, setAddress] = useState(null)
     const [balance, setBalance] = useState(null)
 
-
+    const web3 = new Web3(window.ethereum)
 
     const connectToWallet= async()=> {
         if(window.ethereum) {
             try{
                 await window.ethereum.request({method: 'eth_requestAccounts'});
 
-                const web3 = new Web3(window.ethereum); //insatnce
+                
 
                 const accounts = await web3.eth.getAccounts()   
                 console.log('Connected to MetaMask:', accounts)
                 setAccount(accounts[0])
+                
+                
 
                 const userbal = await web3.eth.getBalance(accounts[0])
-                console.log(userbal)
+                
                 setBalance(userbal)
+                console.log(userbal)
+
             }catch (error){
                 console.error("Metamask not connected")
             }
@@ -35,28 +39,59 @@ const Ui = () => {
         }
     }
 
+  
+
 
     const processPayment= async()=> {
-        if(window.ethereum) {
+        
             try{
-                await window.ethereum.request({method: 'eth_requestAccounts'});
+               
 
-                const web3 = new Web3(window.ethereum); //insatnce
+              
 
                 const payment= await web3.eth.sendTransaction({
                     from: '0xf34b4A56aECE1Ba6Ff15156FDB411261DE483293',
-                    data: '0x8a6bdbb867fe904c4b397dce1e6d2c943da385c9',
+                    to: address,
                     value: web3.utils.toWei('0.1', 'ether'),
                     gas: Number(20000).toString(16),
                     gasPrice: Number(2000000000).toString(16),
 
                     
                   });
+              
 
-                    
+                console.log(payment)
+
+                  
+                }
+            
+                catch (error) {
+                    console.log("error")
+                }
+           
+    }
+
+  
+  return (
+    <>
+   <h1>Connect Metamask Wallet</h1>
+   <button onClick = {connectToWallet}>tap to connect</button>
+
+   <p>Address : {account}</p>
+   <p>Balance: ${balance}</p>
+
+   <form>
+        <input type='text' name= "reciever_address" placeholder='recievers address' onChange={(e) => setAddress(e.target.value)}/>
+   </form>
+   <button onClick={processPayment}>send payment</button>
+  </>
+  )
+}
+
+export default Ui
 
 
-                /*  web3.eth.once('sending', function(payload) {
+/*  web3.eth.once('sending', function(payload) {
                     console.log("Sent")
                   });
               
@@ -84,50 +119,3 @@ const Ui = () => {
                   web3.eth.on('error', function(error) {
                     console.log("error")
                   }); */
-              
-
-                console.log(payment)
-
-                  
-
-
-
-
-
-
-
-                }
-            
-                catch (error) {
-                    console.log("error")
-                }
-            }
-        else{
-            console.log("install metamask")
-        }
-    }
-
-
-    
-
-     
-    
-
-  
-  return (
-    <>
-   <h1>Connect Metamask Wallet</h1>
-   <button onClick = {connectToWallet}>tap to connect</button>
-
-   <p>Address : {account}</p>
-   <p>Balance: ${balance}</p>
-
-   <form>
-        <input type='text' placeholder='recievers address'/>
-   </form>
-   <button onClick={processPayment}>send payment</button>
-  </>
-  )
-}
-
-export default Ui
